872b2ed75d43a350dada1f98762dc3aaabc97104
VitorLima21 <vitor21lima9@gmail.com>
2022-10-13 15:42:35 -0300

    Atualizado

--- user_process.php
+++ user_process.php
@@ -2,7 +2,7 @@
 
   require_once("globals.php");
   require_once("db.php");
-  require_once("models/User.php");
+  require_once("models/Cliente.php");
   require_once("models/Message.php");
   require_once("dao/ClienteController.php");
 
@@ -20,60 +20,25 @@
   if($type === "update") {
 
     // Resgata dados do usuário
-    $userData = $userDao->verifyToken();
+    
 
     // Receber dados do post
-    $name = filter_input(INPUT_POST, "name");
-    $lastname = filter_input(INPUT_POST, "cpf");
-    $email = filter_input(INPUT_POST, "dataNascimento");
-    $bio = filter_input(INPUT_POST, "email");
+    $nome = filter_input(INPUT_POST, "nome");
+    $cpf = filter_input(INPUT_POST, "cpf");
+   
+    $email = filter_input(INPUT_POST, "email");
 
-    // Criar um novo objeto de usuário
-    $user = new User();
+    
+    
 
     // Preencher os dados do usuário
-    $userData->name = $name;
-    $userData->lastname = $lastname;
+    $userData->nome = $nome;
+    $userData->cpf = $cpf;
     $userData->email = $email;
-    $userData->bio = $bio;
+    
 
-    // Upload da imagem
-    if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
-      
-      $image = $_FILES["image"];
-      $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
-      $jpgArray = ["image/jpeg", "image/jpg"];
+///Arrumar
 
-      // Checagem de tipo de imagem
-      if(in_array($image["type"], $imageTypes)) {
-
-        // Checar se jpg
-        if(in_array($image, $jpgArray)) {
-
-          $imageFile = imagecreatefromjpeg($image["tmp_name"]);
-
-        // Imagem é png
-        } else {
-
-          $imageFile = imagecreatefrompng($image["tmp_name"]);
-
-        }
-
-        $imageName = $user->imageGenerateName();
-
-        imagejpeg($imageFile, "./img/users/" . $imageName, 100);
-
-        $userData->image = $imageName;
-
-      } else {
-
-        $message->setMessage("Tipo inválido de imagem, insira png ou jpg!", "error", "back");
-
-      }
-
-    }
-
-    $userDao->update($userData);
 
   // Atualizar senha do usuário
   } else if($type === "changepassword") {
@@ -90,14 +55,8 @@
     if($password == $confirmpassword) {
 
       // Criar um novo objeto de usuário
-      $user = new User();
-
-      $finalPassword = $user->generatePassword($password);
-
-      $user->password = $finalPassword;
-      $user->id = $id;
-
-      $userDao->changePassword($user);
+  
+///Arrumar
 
     } else {
       $message->setMessage("As senhas não são iguais!", "error", "back");
